eb725421b69af1df437cc2d0053f275b8175d0a6
Ben Gelsey <ben@bengelsey.com>
2015-09-28 14:08:37 -0400

    Mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # TODO: Setup Mailer
-  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 80 }
 end
