f2c5393d6cdd1a08f7c0e0f6ecf0e985ef3054d4
Ben Gelsey <ben@bengelsey.com>
2015-09-30 17:36:42 -0400

    Add ADMIN Domain ENV variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # TODO: Setup Mailer
-  config.action_mailer.default_url_options = { host: 'bengelsey.com' }
+  config.action_mailer.default_url_options = { host: ENV.fetch('ADMIN_DOMAIN') }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'in-v3.mailjet.com',
